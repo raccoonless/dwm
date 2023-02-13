5b5dee3799df73d7b76266843527bcd9960862eb
raccoonless <raccoonless@proton.me>
2023-02-12 19:01:27 -0700

    added

--- config.h
+++ config.h
@@ -1,16 +1,21 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
+static const int topbar             = 0;        /* 0 means bottom bar */
+static const char *fonts[]          = { "monospace:size=10", "fontawesome:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
+//background color
+static const char col_gray1[]       = "#000000";
+//inactive window border color
+static const char col_gray2[]       = "#000000";
+//font color
+static const char col_gray3[]       = "#ffffff";
+//current tag and current window font color
 static const char col_gray4[]       = "#ffffff";
+//top bar second color and active window border color
 static const char col_cyan[]        = "#800000";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
@@ -19,7 +24,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
